e65681400a11648d6d5afc1ac0c96b79ea5f1893
Vivek P <vivekpv2004@gmail.com>
2026-07-27 11:33:26 +0530

    Updated the DB

--- Book-My-Ticket-Backend/DataBase/Index.js
+++ Book-My-Ticket-Backend/DataBase/Index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("MongooseAltas Connected.....");
+    console.log("MongoDB Connected Successfully");
   })
   .catch((e) => {
     console.log(e);
